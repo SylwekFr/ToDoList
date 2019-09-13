5d2d7139bd166b17a271940e1dd868b3fc3faa1f
smichel <smichel@nicmangroup.com>
2019-09-13 16:34:27 +0200

    update server.js for been ready

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 var express = require('express');
+var session = require('cookie-session');
+var bodyParser = require('body-parser');
+var encodedUrl = bodyParser.urlencoded({ extended: false });
 
 var app = express();
+app.use(session({secret: 'todolistinsession'}))
 
-app.get('/', function(req, res) {
-    res.render('todolist.ejs');
+.use(function(req, res, next){
+    if (typeof(req.session.todolist) == 'undefined') {
+        req.session.todolist = [];
+    }
+    next();
+})
+.get('/', function(req, res) {
+    res.render('todolist.ejs', {todolist: req.session.todolist});
+})
+.post('/ajouter/', encodedUrl, function(req, res) {
+    if (req.body.addtodo !=''){
+        req.session.todolist.push(req.body.newtodo);
+    }
+    res.redirect('/');
+})
+.get('/supprimer/:index', function(req, res) {
+    if (req.param.index != '') {
+        req.session.todolist.splice(req.params.index, 1);
+    }
+    res.redirect('/');
 })
 .use(function(req, res, next){
-    res.setHeader('Content-Type', 'text/plain');
-    res.status(404).send('404 not found');
+    res.redirect('/')
 });
 app.listen(8080);
