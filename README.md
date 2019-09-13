eb3f0815fa2f87da4d9a7026b976e4a2a81a9f51
smichel <smichel@nicmangroup.com>
2019-09-13 14:53:07 +0200

    initial commit, server on and basic

---
+++ README.md
@@ -0,0 +1,7 @@
+# To Do List  
+Here a to do list application  
+For run this application please go on the root folder open your console and run :  
+`npm install`  
+then :
+`node server.js`
+then open your favorite navigator and go to localhost:8080
---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.render('todolist.ejs');
+})
+.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('404 not found');
+});
+app.listen(8080);
